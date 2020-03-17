7de3301c966dda673bf0e7639055145b4e793483
vantaclaudia <62286274+vantaclaudia@users.noreply.github.com>
2020-03-17 13:14:01 +0200

    Add files via upload
     Sa se creeze o clasa java care seteaza printr-un contructor 2 tipuri de date si le returneaza folosind metodele getter.

---
+++ problema3/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Vehicul v1 = new Vehicul();
+	    v1.setCuloare("Rosu");
+        System.out.println(v1.getCuloare());
+    }
+}
---
+++ problema3/src/com/company/Vehicul.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Vehicul {
+    private String culoare;
+    public String getCuloare() {
+        return culoare;
+    }
+    public void setCuloare(String c) {
+        this.culoare = c;
+    }
+}
